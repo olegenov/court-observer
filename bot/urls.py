5ca44afa7f24d1922cbed3ddc6a3bad8692a4788
Nikita Kitaev <nik_kitaev@mail.ru>
2023-08-21 20:07:04 +0300

    forhot about some shit

--- bot/urls.py
+++ bot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from .views import webhook_handler
+# from .views import webhook_handler
 
 urlpatterns = [
     # path('webhook/', webhook_handler, name='bot_webhook'),
